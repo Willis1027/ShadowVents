e532e1adeccf055263b0920ddbc74cf83d5f8de7
Willis <favorite.ange@gmail.com>
2023-04-13 14:40:56 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template src='./app.html'>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script src='./app.js'>
-
 </script>
 
 <style lang="scss" src=./app.scss>
-
-
-  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).use(store).mount('#app')
+library.add(faPenToSquare,faRotateRight);
+
+createApp(App).use(store).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
